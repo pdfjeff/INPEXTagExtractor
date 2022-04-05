f4a0f67f1428a0e53ba1ef235c07e21bfdf5714b
Jeff Brand <jeff.brand@icloud.com>
2022-04-05 09:50:53 -0400

    Added Example implementation

---
+++ Tag Extractor Example/TagExtractorPrototype/TagExtractorPrototype/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace TagExtractorPrototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnToFile = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Do Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(94, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(800, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "\\b(\\b[A-Za-z]{1}-)?([A-Za-z0-9]{1,7}-)(\\S{1,5}-)(\\S{1,5}-)(\\S{1,5}-)?(\\S{1,5}-)?\\" +
+    "S{1,5}?(\\.[A-Za-z0-9]{1,3})?\\b";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(819, 39);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Hammer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(819, 73);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Clear DB";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnToFile
+            // 
+            this.btnToFile.Location = new System.Drawing.Point(819, 102);
+            this.btnToFile.Name = "btnToFile";
+            this.btnToFile.Size = new System.Drawing.Size(75, 23);
+            this.btnToFile.TabIndex = 6;
+            this.btnToFile.Text = "DB To File";
+            this.btnToFile.UseVisualStyleBackColor = true;
+            this.btnToFile.Click += new System.EventHandler(this.btnToFile_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 42);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(530, 514);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(549, 542);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(819, 132);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Rotate";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(906, 568);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnToFile);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Tag Extractor Prototype";
+            this.Closed += new System.EventHandler(this.Form1_Close);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnToFile;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Tag Extractor Example/TagExtractorPrototype/TagExtractorPrototype/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using iTextSharp.text.pdf;
+
+
+
+// using System.Data.SQLite;
+// The database approach was terribly slow, it seems this implementation of SQLite is not efficient.
+// No worries, we'll use text output instead, it's MUCH faster.
+
+namespace TagExtractorPrototype
+{
+    public partial class Form1 : Form
+    {
+
+       // SQLiteConnection m_dbConnection;
+        int FilesProcessed;
+
+        public Form1()
+
+        {
+            InitializeComponent();
+
+            iTextSharp.text.pdf.PdfReader.unethicalreading = true;
+
+           // m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3");
+           // m_dbConnection.Open();
+            //SQLiteCommand cmd = new SQLiteCommand("PRAGMA journal_mode = WAL", m_dbConnection);
+            //cmd.ExecuteNonQuery();
+            //cmd.CommandText = "PRAGMA synchronous = NORMAL";
+            //cmd.ExecuteNonQuery();
+
+
+            try
+            {
+                //string sql = "CREATE TABLE tags (FileName VARCHAR(255), Tag VARCHAR(255))";
+                //SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+                //command.ExecuteNonQuery();
+            }
+            catch
+            {
+
+                //Error?  Guess we already had the table.
+            }
+
+
+         
+        }
+
+        private void RotatePDF(string inputFile, string outputFile, int desiredRot)
+        {
+
+
+                PdfReader reader = new PdfReader(inputFile);
+                FileStream outStream = new FileStream(outputFile, FileMode.Create);
+
+                int numPages = reader.NumberOfPages;
+
+                PdfDictionary page;
+
+                for (int pageNum = 0; pageNum < numPages; )
+                {
+                    ++pageNum;
+
+                     page = reader.GetPageN(pageNum);
+
+                     PdfNumber rotate = page.GetAsNumber(PdfName.ROTATE);
+
+                    //iTextSharp.text.pdf.PdfNumber rotation = pageDict.GetAsNumber(iTextSharp.text.pdf.PdfName.ROTATE);
+
+
+                    desiredRot += rotate.IntValue;
+                    desiredRot %= 360; // must be 0, 90, 180, or 270
+
+                    page.Put(PdfName.ROTATE, new PdfNumber(desiredRot));
+
+
+                }
+                PdfStamper stamper = new iTextSharp.text.pdf.PdfStamper(reader, outStream);
+                stamper.Close();
+                reader.Close();
+                outStream.Close();
+
+            
+        }
+
+
+        private void Form1_Close(object sender, EventArgs e)
+        {
+            // m_dbConnection.Close();
+        }
+
+
+        private void processFolder()
+        {
+
+
+            string thePath = "\\\\psf\\Dropbox\\Adlib\\Clients\\JCG\\Inpex Tag Extractor\\Samples";
+
+            System.IO.DirectoryInfo theDirectoryInfo = new System.IO.DirectoryInfo(thePath);
+
+            System.IO.FileInfo[] thetxtFiles = theDirectoryInfo.GetFiles("*.txt");
+
+            foreach (System.IO.FileInfo thetxtFile in thetxtFiles)
+            {
+
+               // textBox2.Text += DateTime.Now.ToShortTimeString() + " Opening " + thetxtFile.Name + "\r\n";
+               // Application.DoEvents();
+
+                string txtFile = System.IO.File.ReadAllText(thetxtFile.FullName);
+
+                //Find Matches
+
+                var myRegex = new Regex(textBox1.Text);
+                MatchCollection AllMatches = myRegex.Matches(txtFile);
+                if (AllMatches.Count > 0)
+                {
+
+                    ////We use a transaction to make these record inserts faster.
+                    // SQLiteCommand command = new SQLiteCommand("begin", m_dbConnection);
+                    // command.ExecuteNonQuery();
+
+                    string matchText = "";
+
+                    //string sql = "";
+                    foreach (Match SomeMatch in AllMatches)
+                    {
+
+                        ////Persist this record to the database
+
+                       // sql += "insert into tags (FileName, Tag) VALUES ('";
+                       // sql += thetxtFile.Name.Replace(".txt", "") + "', '";
+                       // sql += SomeMatch.Value + "');\r\n";
+
+                        matchText += thetxtFile.Name.Replace(".txt", "") + "\t" + SomeMatch.Value + "\r\n";
+                    }
+
+
+                    //command.CommandText = sql;
+                    //command.ExecuteNonQuery();
+
+                    System.IO.File.AppendAllText("Tags_txt.txt", matchText);
+
+                    //We use a transaction to make these record inserts faster.
+
+                    //command.CommandText = "END";
+                    //command.ExecuteNonQuery();
+
+                    FilesProcessed += 1;
+                    label1.Text = FilesProcessed + " Files Processed.";
+                    Application.DoEvents(); 
+                }
+
+                //Insert Doc & Matches
+
+            }
+
+            textBox2.Text += DateTime.Now.ToShortTimeString() + " Done.\r\n";
+            textBox2.ScrollToCaret();
+            Application.DoEvents();  
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FilesProcessed = 0;
+            processFolder();
+            }
+        
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            FilesProcessed = 0;
+            //Do the 15 files 11,000 times to simulate 165K files.
+            for (int i = 1; i <= 11000; i++)
+            {
+                processFolder();
+            }
+
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //string sql = "DELETE FROM Tags";
+            //SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            //command.ExecuteNonQuery();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //MessageBox.Show("Form1 Load!");
+        }
+
+        private void btnToFile_Click(object sender, EventArgs e)
+        {
+            //string sql = "select * from tags";
+            //string TagReport = "";
+
+            //SQLiteCommand commandCount = new SQLiteCommand("select count(*) from tags", m_dbConnection);
+            //var RecordCount = commandCount.ExecuteScalar();
+            //SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
+            //SQLiteDataReader reader = command.ExecuteReader();
+            //FilesProcessed = 0;
+            
+
+
+            //while (reader.Read())
+            //{
+            //    TagReport += reader["FileName"] + "\t" + reader["Tag"] + "\r\n";
+            //    FilesProcessed += 1;
+            //    label1.Text = "Retrieved " + FilesProcessed + " records of " + RecordCount + ".";
+            //    Application.DoEvents();
+
+            //}
+            //File.WriteAllText("Tags_DB.txt", TagReport);
+            //label1.Text = "Wrote out " + FilesProcessed + " records.";
+        }
+
+ private void button4_Click(object sender, EventArgs e)
+        {
+       string thePath = @"\\psf\Dropbox\Adlib\Clients\JCG\Inpex Tag Extractor\Samples Provided";
+
+            System.IO.DirectoryInfo theDirectoryInfo = new System.IO.DirectoryInfo(thePath);
+
+            System.IO.FileInfo[] thePDFFiles = theDirectoryInfo.GetFiles("*.pdf");
+
+            foreach (System.IO.FileInfo thePDFFile in thePDFFiles)
+            {
+                RotatePDF(thePDFFile.FullName, (thePath + @"\Input\" +thePDFFile.Name.Replace(".pdf", "+l.pdf")), 270);
+                RotatePDF(thePDFFile.FullName, (thePath + @"\Input\" + thePDFFile.Name.Replace(".pdf", "+r.pdf")), 90);
+                File.Copy(thePDFFile.FullName, (thePath + @"\Input\" + thePDFFile.Name));
+            }
+        }
+    }
+}
